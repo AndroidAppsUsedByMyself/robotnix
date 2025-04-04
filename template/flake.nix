c6d8e907d755aa49df274ca1fc37e6cb5f2a5f09
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-05 04:11:48 +0800

    fix: remove a bug

--- template/flake.nix
+++ template/flake.nix
@@ -31,8 +31,8 @@
       # If you make new changes to your build that you want to be pushed by the OTA updater, you should set this yourself.
       # buildDateTime = 1584398664; # Use `date "+%s"` to get the current time
 
-      # signing.enable = true;
-      # signing.keyStorePath = "/var/secrets/android-keys"; # A _string_ of the path for the key store.
+      signing.enable = true;
+      signing.keyStorePath = "/etc/secrets/android-keys"; # A _string_ of the path for the key store.
 
       # Build with ccache
       # ccache.enable = true;
