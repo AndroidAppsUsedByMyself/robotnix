73f9f15dc0c54a8a51bf991b4611512617ba37b3
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-05 00:53:33 +0800

    feat: add pstar to flake build list

--- template/flake.nix
+++ template/flake.nix
@@ -22,10 +22,25 @@
       # Build with ccache
       # ccache.enable = true;
     });
+    robotnixConfigurations."pstar" = robotnix.lib.robotnixSystem ({ config, pkgs, ... }: {
+      # These two are required options
+      device = "pstar";
+      flavor = "lineageos";
+
+      # buildDateTime is set by default by the flavor, and is updated when those flavors have new releases.
+      # If you make new changes to your build that you want to be pushed by the OTA updater, you should set this yourself.
+      # buildDateTime = 1584398664; # Use `date "+%s"` to get the current time
+
+      # signing.enable = true;
+      # signing.keyStorePath = "/var/secrets/android-keys"; # A _string_ of the path for the key store.
+
+      # Build with ccache
+      # ccache.enable = true;
+    });
 
     # This provides a convenient output which allows you to build the image by
     # simply running "nix build" on this flake.
     # Build other outputs with (for example): "nix build .#robotnixConfigurations.dailydriver.ota"
-    defaultPackage.x86_64-linux = self.robotnixConfigurations."dailydriver".img;
+    defaultPackage.x86_64-linux = self.robotnixConfigurations."pstar".img;
   };
 }
