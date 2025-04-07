402eaa1a7943e55f1cf7425f98d4b7c48cdaf1ce
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-08 00:01:52 +0800

    chore: make my zramswap bigger

--- template/setswap.sh
+++ template/setswap.sh
@@ -1,7 +1,7 @@
 echo '$ sudo swapoff /dev/zram0'
 sudo swapoff /dev/zram0
-echo '$ sudo zramctl --size 32GB --algorithm zstd /dev/zram0'
-sudo zramctl --size 32GB --algorithm zstd /dev/zram0
+echo '$ sudo zramctl --size 40GB --algorithm zstd /dev/zram0'
+sudo zramctl --size 40GB --algorithm zstd /dev/zram0
 echo '$ sudo mkswap /dev/zram0'
 sudo mkswap /dev/zram0
 echo '$ sudo swapon -p 1 /dev/zram0'
