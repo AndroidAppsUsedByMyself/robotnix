1e005f814f71104e8fd339383025bd681b11986c
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-09-07 15:43:37 +0800

    fix: lindroid for 22.1 build failed

--- template/flake.nix
+++ template/flake.nix
@@ -8,10 +8,11 @@
     # zips via the `img` and `ota` attrs, for instance `nix build .#robotnixConfigurations.myLineageOS.ota`.
     robotnixConfigurations."myLineageOS" = robotnix.lib.robotnixSystem ./lineageos.nix;
     robotnixConfigurations."myGrapheneOS" = robotnix.lib.robotnixSystem ./grapheneos.nix;
-    robotnixConfigurations."pstar_lineageos" = robotnix.lib.robotnixSystem ./lineageos-pstar.nix;
+    robotnixConfigurations."pstar_lineageos_22_1" = robotnix.lib.robotnixSystem ./lineageos_22.1_pstar.nix;
+    robotnixConfigurations."pstar_lineageos_22_2" = robotnix.lib.robotnixSystem ./lineageos_22.2_pstar.nix;
 
     # This provides a convenient output which allows you to build the image by
     # simply running "nix build" on this flake.
-    packages.x86_64-linux.default = self.robotnixConfigurations."pstar_lineageos".img;
+    packages.x86_64-linux.default = self.robotnixConfigurations."pstar_lineageos_22_1".img;
   };
 }
---
+++ template/lineageos_22.2_pstar.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  flavor = "lineageos";
+
+  # device codename - FP4 for Fairphone 4 in this case.
+  # Supported devices are listed under https://wiki.lineageos.org/devices/
+  device = "pstar";
+
+  # LineageOS branch.
+  # You can check the supported branches for your device under
+  # https://wiki.lineageos.org/devices/<device codename>
+  # Leave out to choose the official default branch for the device.
+  flavorVersion = "22.2";
+
+  apps.fdroid.enable = true;
+  microg.enable = true;
+
+  # Enables ccache for the build process. Remember to add /var/cache/ccache as
+  # an additional sandbox path to your Nix config.
+  ccache.enable = true;
+
+  # Enable lindroid for the build.
+  lindroid.enable = true;
+  lindroid.systemVersion = "LineageOS_22.1";
+
+  # buildDateTime is set by default by the flavor, and is updated when those flavors have new releases.
+  # If you make new changes to your build that you want to be pushed by the OTA updater, you should set this yourself.
+  # buildDateTime = 1584398664; # Use `date "+%s"` to get the current time
+
+  signing.enable = true;
+  signing.keyStorePath = "/etc/secrets/android-keys"; # A _string_ of the path for the key store.
+}
